7dcdbc42cb055903623bb3a8d5134854772ba8e5
dantoulis <google_for_dantoulis@outlook.com>
2026-05-14 19:49:22 +0300

    Add app viewsets

--- backend/applications/views.py
+++ backend/applications/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import permissions, viewsets
+from .models import Company, JobApplication
+from .serializers import CompanySerializer, JobApplicationSerializer
 
 # Create your views here.
+class CompanyViewSet(viewsets.ModelViewSet):
+    serializer_class = CompanySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Company.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class JobApplicationViewSet(viewsets.ModelViewSet):
+    serializer_class = JobApplicationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return JobApplication.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
