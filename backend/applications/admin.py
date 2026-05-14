88603548bfd76f1fb81db35d5242e488abcc1915
dantoulis <google_for_dantoulis@outlook.com>
2026-05-14 18:52:54 +0300

    Register models in admin panel

--- backend/applications/admin.py
+++ backend/applications/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Company, JobApplication
 
 # Register your models here.
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ["name", "user", "website", "location", "created_at"]
+    search_fields = ["name", "website", "location"]
+    list_filter = ["created_at"]
+
+
+@admin.register(JobApplication)
+class JobApplicationAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "company",
+        "user",
+        "status",
+        "priority",
+        "active",
+        "date_applied",
+        "created_at",
+    ]
+    search_fields = ["title", "company__name", "location"]
+    list_filter = ["status", "priority", "workplace_type", "active", "created_at"]
