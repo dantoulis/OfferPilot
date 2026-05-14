dce0f9edd0fd441339b820ddca9b16101de1586d
dantoulis <google_for_dantoulis@outlook.com>
2026-05-14 19:50:02 +0300

    Create & Add app endpoint urls

--- backend/applications/urls.py
+++ backend/applications/urls.py
@@ -1,2 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CompanyViewSet, JobApplicationViewSet
 
+router = DefaultRouter()
+router.register("companies", CompanyViewSet, basename="company")
+router.register("applications", JobApplicationViewSet, basename="application")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
